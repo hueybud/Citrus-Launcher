d197ce40807848638cfd866e8d23689ded63236b
Brian Huitt <hueybud@gmail.com>
2022-06-28 22:19:23 -0400

    adding made goals canvas into UI

--- matchSettings.js
+++ matchSettings.js
@@ -134,6 +134,7 @@ $(document).ready(function(){
         setBasicLeftGoalTable()
         setBasicRightGoalTable()
         setRuleset()
+        setGoalGraph()
     }
 
     function setBasicLeftItemTable() {
@@ -294,7 +295,7 @@ $(document).ready(function(){
             var goalNumberTD = document.createElement("td");
             goalNumberTD.innerHTML = i + 1;
             var goalTimeTD = document.createElement("td");
-            var timeString = timeToString(jsonStats['Left Team Goal Info'][i])
+            var timeString = timeToString(jsonStats['Left Team Goal Info'][i][0])
             goalTimeTD.innerHTML = timeString;
             localTR.append(goalNumberTD);
             localTR.append(goalTimeTD);
@@ -313,7 +314,7 @@ $(document).ready(function(){
             var goalNumberTD = document.createElement("td");
             goalNumberTD.innerHTML = i + 1;
             var goalTimeTD = document.createElement("td");
-            var timeString = timeToString(jsonStats['Right Team Goal Info'][i])
+            var timeString = timeToString(jsonStats['Right Team Goal Info'][i][0])
             goalTimeTD.innerHTML = timeString;
             localTR.append(goalNumberTD);
             localTR.append(goalTimeTD);
@@ -333,6 +334,93 @@ $(document).ready(function(){
         } else {
             $('#rulsetModalBowser').html(`<span>Bowser Attack : </span><b>${parseInt(jsonStats['Match Bowser or FTX']) ? "On" : "Off"}</b>`)
         }
+    }
+
+    function setGoalGraph() {
+        var coordinates = [[13.981183,-3.400813],
+        [18.110964,3.031374],
+        [1.924551,11.136806],
+        [12.372401,-4.082008],
+        [1.554896,-11.129575],
+        [1.329052,11.130216],
+        [11.875075,-3.724352],
+        [1.376025,-10.842902],
+        [14.192752,2.456968],
+        [13.707708,2.243435],[-13.265551,2.639212],
+        [-7.565029,3.244279],
+        [-11.751383,3.716314],
+        [-14.559775,-1.214220],
+        [-13.421861,3.657639]]
+        const canvasWidth = 600;
+        const canvasHeight = 200;
+        const xWeight = 14.77;
+        const yWeight = -8.48;
+        var canvas = document.getElementById("myCanvas");
+        var ctx = canvas.getContext("2d");
+        for (var i = 0; i < coordinates.length; i++) {
+          ctx.beginPath();
+          ctx.fillStyle = "#00FF00";
+          ctx.arc(transformX(coordinates[i][0]),transformY(coordinates[i][1]),5,0,2*Math.PI);
+          ctx.stroke();
+          ctx.fill();
+        }
+        ctx.moveTo(300, 0);
+        ctx.lineTo(300, 348);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(300,174,51.7,0,2*Math.PI);
+        ctx.stroke();
+        // outer left box
+        ctx.moveTo(transformX(-20.2), transformY(5.2));
+        ctx.lineTo(transformX(-13.5), transformY(5.2));
+        ctx.stroke();
+        ctx.moveTo(transformX(-20.2), transformY(-5.2));
+        ctx.lineTo(transformX(-13.5), transformY(-5.2));
+        ctx.stroke();
+        ctx.moveTo(transformX(-13.5), transformY(-5.2));
+        ctx.lineTo(transformX(-13.5), transformY(5.2));
+        ctx.stroke();
+        // inner left box
+        ctx.moveTo(transformX(-20.2), transformY(2.6));
+        ctx.lineTo(transformX(-17.5), transformY(2.6));
+        ctx.stroke();
+        ctx.moveTo(transformX(-20.2), transformY(-2.6));
+        ctx.lineTo(transformX(-17.5), transformY(-2.6));
+        ctx.stroke();
+        ctx.moveTo(transformX(-17.5), transformY(2.6));
+        ctx.lineTo(transformX(-17.5), transformY(-2.6));
+        ctx.stroke();
+        // needs to be adjusted
+        // outer right box
+        // outer left box
+        ctx.moveTo(transformX(20.2), transformY(5.2));
+        ctx.lineTo(transformX(13.5), transformY(5.2));
+        ctx.stroke();
+        ctx.moveTo(transformX(20.2), transformY(-5.2));
+        ctx.lineTo(transformX(13.5), transformY(-5.2));
+        ctx.stroke();
+        ctx.moveTo(transformX(13.5), transformY(-5.2));
+        ctx.lineTo(transformX(13.5), transformY(5.2));
+        ctx.stroke();
+        // inner left box
+        ctx.moveTo(transformX(20.2), transformY(2.6));
+        ctx.lineTo(transformX(17.5), transformY(2.6));
+        ctx.stroke();
+        ctx.moveTo(transformX(20.2), transformY(-2.6));
+        ctx.lineTo(transformX(17.5), transformY(-2.6));
+        ctx.stroke();
+        ctx.moveTo(transformX(17.5), transformY(2.6));
+        ctx.lineTo(transformX(17.5), transformY(-2.6));
+        ctx.stroke();
+        //ctx.font = "20px Helvetica Neue";
+        //ctx.strokeText("1", 295, 105);
+    }
+
+    function transformX(input) {
+        return (input * 14.77) + 300
+    }
+    function transformY(input) {
+        return (input * -14.77) + 174
     }
 
     $('#rulesetElement').on('click', function(){
